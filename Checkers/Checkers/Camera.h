30a6871f2c524530cb7bbd37ad2932c2d8edda22
Daniel Tenorio <tnr.daniel@gmail.com>
2017-06-08 21:29:25 -0700

    Begin stages of camera boundaries

--- Checkers/Checkers/Camera.h
+++ Checkers/Checkers/Camera.h
@@ -9,11 +9,26 @@ struct Camera
 	GLfloat positionY = 0;
 	GLfloat offsetX = 0;
 	GLfloat offsetY = 0;
+	GLfloat minX = 0;
+	GLfloat minY = 0;
 	GameObject* objectToFollow;
 
 	void render()
 	{
 		glLoadIdentity();
-		glTranslatef(-objectToFollow->PositionX() + offsetX, -objectToFollow->PositionY() + offsetY, 0.f);
+		
+		GLfloat objectPositionX = objectToFollow->PositionX();
+		GLfloat objectPositionY = objectToFollow->PositionY();
+
+		if (objectPositionX > minX + offsetX)
+		{
+			glTranslatef(-objectToFollow->PositionX() + offsetX, 0.f, 0.f);
+		}
+		
+		if (objectPositionY > minY + offsetY)
+		{
+			glTranslatef(0.f, -objectToFollow->PositionY() + offsetY, 0.f);
+		}
+
 	}
 };
